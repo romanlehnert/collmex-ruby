7bfee3407035f39e386689ee6c8a6b6aba83bb84
Roman <roman.lehnert@googlemail.com>
2012-07-21 12:42:39 +0200

    started integration test

--- spec/lib/collmex_spec.rb
+++ spec/lib/collmex_spec.rb
@@ -16,4 +16,27 @@ describe Collmex do
 end
 
 
+describe "CollmexIntegration" do
+
+  before(:each) do 
+    setup_login_data
+  end
+
+  it "should build sample output" do
+    login = Collmex::Api::Login.new({ username: Collmex.username, password: Collmex.password })
+    customer_get = Collmex::Api::CustomerGet.new( {:customer_id => 9999} )
+    #puts login
+    #puts customer
+
+    request = Collmex::Request.new
+    request.add_command customer_get
+  end
+
+  
+
+  
+
+
+end
+
 
