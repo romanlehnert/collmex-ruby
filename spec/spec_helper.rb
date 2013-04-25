04db4a021713ec1a5451ad31296b859a329dd170
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2013-04-25 12:06:05 +0200

    more code/file cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
-require 'rspec'
+require "rspec"
 require "awesome_print"
 require "vcr"
 
 RSpec.configure do |config|
-
   config.color_enabled = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
@@ -16,11 +15,9 @@ RSpec.configure do |config|
   config.after(:each) do
     Collmex.reset_login_data
   end
-
 end
 
-
-def timed(name)  
+def timed(name)
   start = Time.now
   puts "\n[STARTED: #{name}]"
   yield if block_given?
@@ -28,10 +25,9 @@ def timed(name)
   puts "[FINISHED: #{name} in #{(finish - start) * 1000} milliseconds]"
 end
 
-
 VCR.configure do |c|
   c.allow_http_connections_when_no_cassette = true
-  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   c.hook_into :webmock
 end
 
